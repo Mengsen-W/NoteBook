a761709e627e798f3b035214a684c5cf735d4bcc
Mengsen-W <mengsen_wang@163.com>
2020-03-21 11:48:11 +0800

    lambda

--- C_Cpp/Example/Lambda.cpp
+++ C_Cpp/Example/Lambda.cpp
@@ -2,33 +2,38 @@
  * @Author: Mengsen.Wang
  * @Date: 2020-03-21 11:21:12
  * @Last Modified by: Mengsen.Wang
- * @Last Modified time: 2020-03-21 11:31:18
+ * @Last Modified time: 2020-03-21 11:44:06
  */
 
 #include <functional>
 #include <iostream>
 
-const void printInfo(int a, int b, int c) {
+void printInfo(const int a, const int b, const int c) {
   std::cout << "a = " << a << " b = " << b << " c = " << c << std::endl;
 }
 
 struct Print {
-  void operator()(int a, int b, int c) const {
+  void operator()(const int a, const int b, const int c) const {
     std::cout << "a = " << a << " b = " << b << " c = " << c << std::endl;
   }
 };
 
 template <typename T1, typename T2, typename T3>
-void templatePrint(T1 a, T2 b, T3 c) {
+void templatePrint(const T1 a, const T2 b, const T3 c) {
   std::cout << "a = " << a << " b = " << b << " c = " << c << std::endl;
 }
 
 struct TemplatePrint {
   template <typename T1, typename T2, typename T3>
-  void operator()(T1 a, T2 b, T3 c) {
+  void operator()(const T1 a, const T2 b, const T3 c) const {
     std::cout << "a = " << a << " b = " << b << " c = " << c << std::endl;
   }
 };
+
+template <typename Func>
+void printUseFunc(Func func, const int a, const int b, const int c) {
+  func(a, b, c);
+}
 
 int main() {
   Print printUseClass;
@@ -38,5 +43,18 @@ int main() {
   printUseClass(1, 2, 3);
   templatePrint(1, 2, 3);
   printUseTemplate(1, 2, 3);
+
+  // lambad
+  // local
+  auto local = [](const int a, const int b, const int c) {
+    std::cout << "a = " << a << " b = " << b << " c = " << c << std::endl;
+  };
+
+  printUseFunc(
+      [](const int a, const int b, const int c) {
+        std::cout << "a = " << a << " b = " << b << " c = " << c << std::endl;
+      },
+      1, 2, 3);
+
   return 0;
 }

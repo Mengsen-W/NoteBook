dec89383036d60ad121eb4467321123205802298
Mengsen-W <mengsen_wang@163.com>
2020-03-21 12:28:11 +0800

    STL

---
+++ C_Cpp/Example/STLKownledge.cpp
@@ -0,0 +1,37 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2020-03-21 12:00:17
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2020-03-21 12:20:14
+ */
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <list>
+#include <map>
+#include <random>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+static void vercorPart();
+static void listPart();
+static void mapPart();
+static void setPart();
+static void unorderedMapPart();
+static void unorderedSetPart();
+static void stlAlgorithm();
+
+int main() {
+  vercorPart();
+  listPart();
+  mapPart();
+  setPart();
+  unorderedSetPart();
+  unorderedMapPart();
+  stlAlgorithm();
+
+  return 0;
+}
---
+++ C_Cpp/STLKnowledge.md
@@ -0,0 +1,27 @@
+# 容器/迭代器/算法
+---
+---
+1. ## 顺序容器--数组或指针--遍历快空间小
+   1. array
+   2. string
+   3. vector
+   4. deque
+   5. stack
+   6. queue
+   7. priority_queue
+   8. list
+   9. forward_list
+   10. bitset
+---
+2. ## 关联容器--平衡二叉树--排序查找块
+   1. set--红黑树
+   2. map--红黑树
+   3. multiset
+   4. multimap
+---
+3. ## 无序容器--hash_table--遍历快查找快
+   1. unordered_set
+   2. unordered_map
+   3. unordered_multimap
+   4. unordered_multiset
+---
